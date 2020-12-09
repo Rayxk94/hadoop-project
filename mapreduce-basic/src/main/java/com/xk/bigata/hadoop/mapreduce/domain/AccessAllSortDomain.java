2cf1847df1d2f019f0fd926d5f23f84eb518c6a1
xiekai <xk19941011>
2020-12-09 13:48:04 +0800

    add AllSortDriver

---
+++ mapreduce-basic/src/main/java/com/xk/bigata/hadoop/mapreduce/domain/AccessAllSortDomain.java
@@ -0,0 +1,97 @@
+package com.xk.bigata.hadoop.mapreduce.domain;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * 自定义数据类型步骤：
+ * 1. 实现WritableComparable/Writable接口
+ * 2. 重写 write、readFields、compareTo方法
+ * 3. 要有一个无参的构造器
+ * 4. write和readFields的字段顺序要保持一致
+ * 5. 输出结果：需要重写toString(非必选)
+ */
+public class AccessAllSortDomain implements WritableComparable<AccessAllSortDomain> {
+
+    private String phone;
+
+    private Long up;
+
+    private Long down;
+
+    private Long sum;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Long getUp() {
+        return up;
+    }
+
+    public void setUp(Long up) {
+        this.up = up;
+    }
+
+    public Long getDown() {
+        return down;
+    }
+
+    public void setDown(Long down) {
+        this.down = down;
+    }
+
+    public Long getSum() {
+        return sum;
+    }
+
+    public void setSum(Long sum) {
+        this.sum = sum;
+    }
+
+    public AccessAllSortDomain() {
+    }
+
+    public AccessAllSortDomain(String phone, Long up, Long down, Long sum) {
+        this.phone = phone;
+        this.up = up;
+        this.down = down;
+        this.sum = sum;
+    }
+
+    @Override
+    public String toString() {
+        return phone + '\t' +
+                up + '\t' +
+                down + '\t' +
+                sum;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeUTF(phone);
+        out.writeLong(up);
+        out.writeLong(down);
+        out.writeLong(sum);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        phone = in.readUTF();
+        up = in.readLong();
+        down = in.readLong();
+        sum = in.readLong();
+    }
+
+    @Override
+    public int compareTo(AccessAllSortDomain o) {
+        return this.getSum() > o.getSum() ? -1 : 1;
+    }
+}
---
+++ mapreduce-basic/src/main/java/com/xk/bigata/hadoop/mapreduce/sort/AllSortDriver.java
@@ -0,0 +1,79 @@
+package com.xk.bigata.hadoop.mapreduce.sort;
+
+import com.xk.bigata.hadoop.mapreduce.domain.AccessAllSortDomain;
+import com.xk.bigata.hadoop.utils.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class AllSortDriver {
+
+    public static void main(String[] args) throws Exception {
+
+        String input = "mapreduce-basic/data/sort.data";
+        String output = "mapreduce-basic/out";
+
+        // 1 创建 MapReduce job
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+
+        // 删除输出路径
+        FileUtils.deleteFile(job.getConfiguration(), output);
+
+        // 2 设置运行主类
+        job.setJarByClass(AllSortDriver.class);
+
+        // 3 设置Map和Reduce运行的类
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(MyReducer.class);
+
+        // 4 设置Map 输出的 KEY 和 VALUE 数据类型
+        job.setMapOutputKeyClass(AccessAllSortDomain.class);
+        job.setMapOutputValueClass(Text.class);
+
+        // 5 设置Reduce 输出 KEY 和 VALUE 数据类型
+        job.setOutputKeyClass(AccessAllSortDomain.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        // 6 设置输入和输出路径
+        FileInputFormat.setInputPaths(job, new Path(input));
+        FileOutputFormat.setOutputPath(job, new Path(output));
+
+        job.setNumReduceTasks(1);
+
+        // 7 提交job
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+    }
+
+    public static class MyMapper extends Mapper<LongWritable, Text, AccessAllSortDomain, Text> {
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String[] spilts = value.toString().split("\t");
+            String phone = spilts[0];
+            long up = Long.parseLong(spilts[1]);
+            long down = Long.parseLong(spilts[2]);
+            long sum = Long.parseLong(spilts[3]);
+            context.write(new AccessAllSortDomain(phone, up, down, sum), new Text(phone));
+        }
+    }
+
+    public static class MyReducer extends Reducer<AccessAllSortDomain, Text, AccessAllSortDomain, NullWritable> {
+        @Override
+        protected void reduce(AccessAllSortDomain key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            for (Text value : values) {
+                context.write(key, NullWritable.get());
+            }
+        }
+    }
+}
